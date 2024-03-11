4f600b75764858fa18c3187084b5afd45c4e22f5
Lee Baker <lee@inventory2invoice.com>
2024-03-11 16:19:40 +0000

    End of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './models/product';
-import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +8,11 @@ import { Pagination } from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: Product[] = [];
+ 
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
   
   ngOnInit(): void {
-    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pagesize=50').subscribe({
-      next: response => this.products = response.data, //What to do next
-      error: error => console.log(error), //What to do if error
-      complete: () => {
-        console.log('Request Completed');
-        console.log('Extra Statement');
-      }
-    })
+
   }
 }
